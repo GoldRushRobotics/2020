2216e2e2a1e46efa656604404ba73fe4fb425341
lucastherrien <ltherrien19@rthighschool.org>
2019-10-14 18:36:39 -0400

    Removed redundant code and added limit switch

--- ButtonPushing/Sheild_Solenoid_Control/Sheild_Solenoid_Control.ino
+++ ButtonPushing/Sheild_Solenoid_Control/Sheild_Solenoid_Control.ino
@@ -5,75 +5,18 @@
 // Tle94112 Object
 Tle94112 controller = Tle94112();
 
-// Tle94112Motor Objects
-Tle94112Motor solenoid0(controller);
-Tle94112Motor solenoid1(controller);
-Tle94112Motor solenoid2(controller);
-Tle94112Motor solenoid3(controller);
-Tle94112Motor solenoid4(controller);
-Tle94112Motor solenoid5(controller);
-Tle94112Motor solenoid6(controller);
-Tle94112Motor solenoid7(controller);
-Tle94112Motor solenoid8(controller);
-Tle94112Motor solenoid9(controller);
-
-
 void setup() {
   // Enable MotorController Tle94112
   // Note: Required to be done before starting to configure the solenoid
   controller.begin();
 
   Serial.begin(9600);
-  
-  // Connect solenoid1 to HB1 and so on...
-  solenoid1.connect(solenoid1.HIGHSIDE, controller.TLE_HB1);
-  
-  solenoid2.connect(solenoid2.HIGHSIDE, controller.TLE_HB2);
 
-  solenoid3.connect(solenoid3.HIGHSIDE, controller.TLE_HB3);
-  
-  solenoid4.connect(solenoid4.HIGHSIDE, controller.TLE_HB4);
-
-  solenoid5.connect(solenoid5.HIGHSIDE, controller.TLE_HB5);
-  
-  solenoid6.connect(solenoid6.HIGHSIDE, controller.TLE_HB6);
-
-  solenoid7.connect(solenoid7.HIGHSIDE, controller.TLE_HB7);
-
-  solenoid8.connect(solenoid8.HIGHSIDE, controller.TLE_HB8);
-
-  solenoid9.connect(solenoid9.HIGHSIDE, controller.TLE_HB9);
-
-  solenoid0.connect(solenoid0.HIGHSIDE, controller.TLE_HB10);
-  
-  //when configuration is done, call begin to start operating the solenoids
-  solenoid0.begin();
-  solenoid1.begin();
-  solenoid2.begin();
-  solenoid3.begin();
-  solenoid4.begin();
-  solenoid5.begin();
-  solenoid6.begin();
-  solenoid7.begin();
-  solenoid8.begin();
-  solenoid9.begin();
-  
-  //after calling begin(), the solenoids are active. 
+  pinMode(7, INPUT);
 }
 
 
 void loop() {
-  solenoid0.start(0);
-  solenoid1.start(0);
-  solenoid2.start(0);
-  solenoid3.start(0);
-  solenoid4.start(0);
-  solenoid5.start(0);
-  solenoid6.start(0);
-  solenoid7.start(0);
-  solenoid8.start(0);
-  solenoid9.start(0);
-
   controller.writeReg(Tle94112::PWM1_DC_CTRL, 0x03, 0xFF, 255);
   controller.writeReg(Tle94112::PWM2_DC_CTRL, 0x03, 0xFF, 0);
   
@@ -84,6 +27,8 @@ void loop() {
       //String s = "0123456789";
       //char c = '0'; 
       //Serial.println(c);
+
+      while(digitalRead(7) != HIGH){}
       
       if(c == '0'){
         controller.configHB(controller.TLE_HB10, controller.TLE_HIGH, controller.TLE_NOPWM, 0);
